810218ab7bf84b62467ed66bea335904bc7d70e4
GRRais <garifzyanov.rr@gmail.com>
2021-04-20 16:30:03 +0300

    lesson 1 14.30

---
+++ LessonSQLiteKotlin/app/src/main/java/ru/rayanis/lessonsqlitekotlin/EditActivity.kt
@@ -0,0 +1,11 @@
+package ru.rayanis.lessonsqlitekotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_activity)
+    }
+}
