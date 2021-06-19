68b2268cdd3ea96b38c91e4f1ec5ebed5372e43b
GRRais <garifzyanov.rr@gmail.com>
2021-06-19 16:40:26 +0300

    lesson 2 34.40 stroi

--- stroyka/app/src/main/java/ru/rayanis/stroyka/MainActivity.kt
+++ stroyka/app/src/main/java/ru/rayanis/stroyka/MainActivity.kt
@@ -2,10 +2,24 @@ package ru.rayanis.stroyka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import ru.rayanis.stroyka.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var b: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        val view = b.root
+        setContentView(view)
+
+        init()
+
+    }
+
+    private fun init() {
+        var toggle = ActionBarDrawerToggle(this, b.drawerLayout, b.)
     }
 }
