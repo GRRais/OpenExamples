015b680dd9bf818091b011c26bbcc588f38ad635
GRRais <garifzyanov.rr@gmail.com>
2021-07-15 21:46:40 +0300

    13 3,30

--- stroyka/app/src/main/java/ru/rayanis/stroyka/act/EditObjectsAct.kt
+++ stroyka/app/src/main/java/ru/rayanis/stroyka/act/EditObjectsAct.kt
@@ -2,8 +2,10 @@ package ru.rayanis.stroyka.act
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import ru.rayanis.stroyka.R
 import ru.rayanis.stroyka.databinding.ActivityEditObjectsBinding
+import ru.rayanis.stroyka.utils.VillageHelper
 
 class EditObjectsAct : AppCompatActivity() {
     private lateinit var b : ActivityEditObjectsBinding
@@ -12,5 +14,10 @@ class EditObjectsAct : AppCompatActivity() {
         setContentView(R.layout.activity_edit_objects)
         b = ActivityEditObjectsBinding.inflate(layoutInflater)
         setContentView(b.root)
+        val listAreas = VillageHelper.getAllAreas(this)
+        val adapter = ArrayAdapter(this,
+            android.R.layout.simple_spinner_item, VillageHelper.getAllAreas(this))
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        b.spArea.adapter = adapter
     }
 }
---
+++ stroyka/app/src/main/java/ru/rayanis/stroyka/dialogs/DialogSpinnerHelper.kt
@@ -0,0 +1,7 @@
+package ru.rayanis.stroyka.dialogs
+
+import android.content.Context
+
+class DialogSpinnerHelper {
+    fun showSpinnerDialog(context: Context, list: ArrayList<String>) {}
+}
