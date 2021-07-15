9e40b18b6839d8481cf7f5457088b1618466277c
GRRais <garifzyanov.rr@gmail.com>
2021-07-15 05:42:05 +0300

    L12 23.45

--- stroyka/app/src/main/java/ru/rayanis/stroyka/utils/VillageHelper.kt
+++ stroyka/app/src/main/java/ru/rayanis/stroyka/utils/VillageHelper.kt
@@ -1,11 +1,26 @@
 package ru.rayanis.stroyka.utils
 
+import android.content.Context
+import org.json.JSONObject
 import java.io.IOException
 
 object VillageHelper {
-    fun getAllAreas(): ArrayList<String> {
+    fun getAllAreas(context: Context): ArrayList<String> {
         var tempArray = ArrayList<String>()
         try {
+
+            val inputStream = context.assets.open("areasToVillages.json")
+            val size: Int = inputStream.available()
+            val bytesArray = ByteArray(size)
+            inputStream.read(bytesArray)
+            val jsonFile = String(bytesArray)
+            val jsonObject = JSONObject(jsonFile)
+            val areasNames = jsonObject.names()
+            if (areasNames != null) {
+                for (n in 0 until areasNames.length()) {
+                    tempArray.add(areasNames.getString(n))
+                }
+            }
 
         } catch (e: IOException) {
 
