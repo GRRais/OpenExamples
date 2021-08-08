ea4e4aa231ba974e8bf9f30d00758f019e4fe6bf
GRRais <garifzyanov.rr@gmail.com>
2021-08-08 22:54:10 +0300

    L26 ready

--- stroyka/app/src/main/java/ru/rayanis/stroyka/utils/ImageManager.kt
+++ stroyka/app/src/main/java/ru/rayanis/stroyka/utils/ImageManager.kt
@@ -1,6 +1,8 @@
 package ru.rayanis.stroyka.utils
 
 import android.graphics.BitmapFactory
+import androidx.exifinterface.media.ExifInterface
+import java.io.File
 
 object ImageManager {
     fun getImageSize(uri: String): List<Int>{
@@ -8,6 +10,21 @@ object ImageManager {
             inJustDecodeBounds = true
         }
         BitmapFactory.decodeFile(uri, options)
-        return listOf(options.outWidth, options.outHeight)
+        return if (imageRotation(uri) == 90)
+            listOf(options.outHeight, options.outWidth)
+        else listOf(options.outWidth, options.outHeight)
+
     }
+     private fun imageRotation(uri: String): Int {
+         val rotation: Int
+         val imageFile = File(uri)
+         val exif = ExifInterface(imageFile.absolutePath)
+         val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+         rotation = if (orientation == ExifInterface.ORIENTATION_ROTATE_90 || orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+             90
+         } else {
+             0
+         }
+         return rotation
+     }
 }
