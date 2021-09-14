fdad8aa226e4cf1ed41a2f0576a73f5aec097acc
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2021-09-14 21:59:26 +0300

    RRChat lesson 2 have done

--- RRChat/app/src/main/java/ru/rayanis/rrchat/MainActivity.kt
+++ RRChat/app/src/main/java/ru/rayanis/rrchat/MainActivity.kt
@@ -2,17 +2,43 @@ package ru.rayanis.rrchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import ru.rayanis.rrchat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var b: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
 
-        val database = Firebase.database("https://rrchat-334c4-default-rtdb.europe-west1.firebasedatabase.app")
+        val database = Firebase.database
         val myRef = database.getReference("message")
+        b.bSend.setOnClickListener {
+            myRef.setValue(b.edMessage.text.toString())
+        }
+        onChangeListener(myRef)
+    }
+    private fun onChangeListener(dRef: DatabaseReference) {
+        dRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                b.apply {
+                    rcView.append("\n")
+                    rcView.append(snapshot.value.toString())
+                }
+            }
 
-        myRef.setValue("Hello, World!")
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
