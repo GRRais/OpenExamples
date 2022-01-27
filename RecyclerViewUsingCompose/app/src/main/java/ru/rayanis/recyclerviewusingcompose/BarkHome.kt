30b09209fd890e59e966bebc530223dab8469e59
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2022-01-27 21:15:19 +0300

    Finished part 1 RecyclerView Compose

---
+++ RecyclerViewUsingCompose/app/src/main/java/ru/rayanis/recyclerviewusingcompose/BarkHome.kt
@@ -0,0 +1,24 @@
+package ru.rayanis.recyclerviewusingcompose
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.items
+import ru.rayanis.recyclerviewusingcompose.data.DataProvider
+
+
+@Composable
+fun BarkHomeContent() {
+    val puppies = remember { DataProvider.puppyList }
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        items(
+            items = puppies,
+            itemContent = {
+                PuppyListItem(puppy = it)
+            })
+    }
+}
