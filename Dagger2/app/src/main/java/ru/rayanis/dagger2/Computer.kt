893ad97324b37451aa8a486ff23d37c1bea2ce4a
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2022-03-27 13:11:22 +0300

    learning dagger by cirill rozov

---
+++ Dagger2/app/src/main/java/ru/rayanis/dagger2/Computer.kt
@@ -0,0 +1,19 @@
+package ru.rayanis.dagger2
+
+class Processor {
+    override fun toString() = "AB2021"
+}
+
+class Motherboard {
+    override fun toString() = "X7 3000"
+}
+
+class RAM {
+    override fun toString() = "16 GB"
+}
+
+data class Computer(
+    val processor: Processor ,
+    val motherboard: Motherboard ,
+    val ram: RAM
+)
---
+++ Dagger2/app/src/main/java/ru/rayanis/dagger2/Main.kt
@@ -0,0 +1,5 @@
+package ru.rayanis.dagger2
+
+fun main() {
+    val appComponent: AppComponent = DaggerAppComponent.create()
+}
