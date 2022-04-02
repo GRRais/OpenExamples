99f4b7e0f53f44ac1bb21a29cb47c023e234101e
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2022-04-02 09:13:18 +0300

    changes

--- Dagger2/app/src/main/java/ru/rayanis/dagger2/Main.kt
+++ Dagger2/app/src/main/java/ru/rayanis/dagger2/Main.kt
@@ -1,6 +1,10 @@
 package ru.rayanis.dagger2
 
 fun main() {
+<<<<<<< Updated upstream
     val appComponent: AppComponent = DaggerAppComponent.create()
     print(appComponent.computer)
+=======
+//    val appComponent: AppComponent = DaggerAppComponent.create()
+>>>>>>> Stashed changes
 }
